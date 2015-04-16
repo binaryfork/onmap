f3c80abd2b2b5d381f7a1cab93008cf9ba121cd8
binaryfork <binaryfork@gmail.com>
2015-04-17 01:33:06 +0600

    Update to Timber 3.0.0

--- app/src/main/java/com/binaryfork/onmap/BaseApplication.java
+++ app/src/main/java/com/binaryfork/onmap/BaseApplication.java
@@ -3,9 +3,6 @@ package com.binaryfork.onmap;
 import android.app.Application;
 import android.content.Context;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import timber.log.Timber;
 
 public class BaseApplication extends Application {
@@ -29,57 +26,15 @@ public class BaseApplication extends Application {
     }
 
     private static class LineNumberTree extends Timber.DebugTree {
-        private static final Pattern ANONYMOUS_CLASS = Pattern.compile("\\$\\d+$");
-
-        @Override
-        protected String createTag() {
-            String tag = nextTag();
-            if (tag != null) {
-                return tag;
-            }
-
-            // DO NOT switch this to Thread.getCurrentThread().getStackTrace(). The test will pass
-            // because Robolectric runs them on the JVM but on Android the elements are different.
-            StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-            if (stackTrace.length < 6) {
-                throw new IllegalStateException(
-                        "Synthetic stacktrace didn't have enough elements: are you using proguard?");
-            }
-            tag = stackTrace[5].getClassName();
-            String lineNumber = ":" + stackTrace[5].getLineNumber();
-
-            Matcher m = ANONYMOUS_CLASS.matcher(tag);
-            if (m.find()) {
-                tag = m.replaceAll("");
-            }
-            return tag.substring(tag.lastIndexOf('.') + 1) + lineNumber;
+        @Override protected String createStackElementTag(StackTraceElement element) {
+            return super.createStackElementTag(element) + ":" + element.getLineNumber();
         }
     }
 
-    /**
-     * A tree which logs important information for crash reporting.
-     */
-    private static class CrashReportingTree extends Timber.HollowTree {
-        @Override
-        public void i(String message, Object... args) {
-            // TODO e.g., Crashlytics.log(String.format(message, args));
-        }
-
-        @Override
-        public void i(Throwable t, String message, Object... args) {
-            i(message, args); // Just add to the log.
-        }
-
-        @Override
-        public void e(String message, Object... args) {
-            i("ERROR: " + message, args); // Just add to the log.
-        }
-
-        @Override
-        public void e(Throwable t, String message, Object... args) {
-            e(message, args);
-
-            // TODO e.g., Crashlytics.logException(t);
+    /** A tree which logs important information for crash reporting. */
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override protected void log(int priority, String tag, String message, Throwable t) {
+            // Add Crashlytics reporting.
         }
     }
 }
